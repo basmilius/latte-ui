8d67b6bf7b2828583334215bfb998229a3e55423
Bas Milius <basmilius@hotmail.com>
2018-11-02 13:27:12 +0100

    Added latte-json-editor.

---
+++ src/vue/component/JsonEditorIaf.vue
@@ -0,0 +1,96 @@
+<template>
+
+	<div class="json-editor json-editor-iaf">
+		<input class="form-control" id="iaf-key" type="text" v-model="keyName" placeholder="Key" v-if="needName"/>
+
+		<select class="custom-select ml-3" id="iaf-type" v-model="formatSelected">
+			<option :value="item" v-for="(item, index) in formats" :key="index">{{ item }}</option>
+		</select>
+
+		<template v-if="formatSelected !== 'array' && formatSelected !== 'object' && formatSelected !== 'null'">
+			<input class="form-control ml-3" :id="'iaf-value'" type="text" v-model="valueName" placeholder="Value" v-if="formatSelected === 'string'"/>
+			<input class="form-control ml-3" :id="'iaf-value'" type="number" v-model="valueName" placeholder="Value" v-if="formatSelected === 'number'" @change="dealNumber"/>
+			<select v-model="valueName" class="custom-select ml-3" :id="'iaf-value'" v-if="formatSelected === 'boolean'" @change="dealBoolean">
+				<option value="true" selected>TRUE</option>
+				<option value="true">FALSE</option>
+			</select>
+		</template>
+
+		<button class="btn btn-primary ml-3" @click="confirm"><i class="mdi mdi-check-circle"></i></button>
+		<button class="btn btn-light ml-1" @click="cancel"><i class="mdi mdi-close-circle"></i></button>
+	</div>
+
+</template>
+
+<script>
+
+	export default {
+
+		name: "latte-json-editor-iaf",
+
+		props: {
+
+			needName: {
+				default: true
+			}
+
+		},
+
+		data()
+		{
+			return {
+				formats: ["string", "array", "object", "number", "boolean", "null"],
+				formatSelected: "string",
+				keyName: "",
+				valueName: ""
+			};
+		},
+
+		methods: {
+
+			confirm()
+			{
+				let value = null;
+
+				if (this.formatSelected === "array")
+					value = [];
+				else if (this.formatSelected === "null")
+					value = null;
+				else if (this.formatSelected === "object")
+					value = [];
+				else
+					value = this.valueName;
+
+				let objectData = {
+					key: this.needName ? this.keyName : null,
+					value: value,
+					type: this.formatSelected
+				};
+
+				this.$emit("confirm", objectData);
+
+				this.keyName = "";
+				this.valueName = "";
+				this.formatSelected = "string";
+			},
+
+			cancel()
+			{
+				this.$emit("cancel");
+			},
+
+			dealBoolean()
+			{
+				this.valueName = Boolean(this.valueName);
+			},
+
+			dealNumber()
+			{
+				this.valueName = Number(this.valueName);
+			}
+
+		}
+
+	}
+
+</script>
---
+++ src/vue/component/JsonEditorView.vue
@@ -0,0 +1,148 @@
+<template>
+
+	<div class="json-editor json-editor-view">
+		<div class="json-editor json-editor-block" v-for="(item, index) in flowData" :key="key(item)" :class="[item.type, {'hide-block': hideMyBlock[index]}]">
+			<div class="key">
+				<button class="btn btn-light toggle" v-if="item.type === 'object' || item.type === 'array'" @click="toggleBlock(index)"><i :class="['mdi', {'mdi-chevron-down': !hideMyBlock[index]}, {'mdi-chevron-right': hideMyBlock[index]}]"></i></button>
+				<input type="text" v-model="item.name" class="form-control" v-if="typeof item.name === 'string'" @blur="keyInputBlur(item, $event)"/>
+				<span class="json-editor json-editor-o" v-if="item.type === 'array'">{{ "[" + item.childParams.length + "]" }}</span>
+				<span class="json-editor json-editor-o" v-if="item.type === 'object'">{{ "{" + (item.childParams.length || 0) + "}" }}</span>
+				<button class="btn btn-light trash" @click="deleteItem(parsedData, item, index)" v-if="item.type === 'object' || item.type === 'array'"><i class="mdi mdi-delete"></i></button>
+			</div>
+			<div class="value">
+				<template v-if="item.type === 'object'">
+					<latte-json-editor-view :parsedData="item.childParams" v-model="item.childParams"></latte-json-editor-view>
+				</template>
+				<template v-else-if="item.type === 'array'">
+					<latte-json-editor-view-array :parsedData="item.childParams" v-model="item.childParams"></latte-json-editor-view-array>
+				</template>
+				<template v-else>
+					<input type="text" v-model="item.remark" class="form-control" v-if="item.type === 'string'">
+					<input type="number" v-model.number="item.remark" class="form-control" v-if="item.type === 'number'">
+					<select name="value" v-model="item.remark" class="custom-select" v-if="item.type === 'boolean'">
+						<option :value="true">TRUE</option>
+						<option :value="false">FALSE</option>
+					</select>
+					<button class="btn btn-light trash" @click="deleteItem(parsedData, item, index)"><i class="mdi mdi-delete"></i></button>
+				</template>
+			</div>
+		</div>
+		<latte-json-editor-iaf v-if="isAddingItem" @confirm="newItem" @cancel="cancelNewItem"></latte-json-editor-iaf>
+		<button class="btn btn-light" @click="addItem" v-if="!isAddingItem"><i class="mdi mdi-plus-circle"></i></button>
+	</div>
+
+</template>
+
+<script>
+
+	export default {
+
+		name: "latte-json-editor-view",
+
+		props: {
+
+			parsedData: {
+				required: true
+			}
+
+		},
+
+		created()
+		{
+			this.flowData = this.parsedData;
+		},
+
+		data()
+		{
+			return {
+				flowData: [],
+				hideMyBlock: {},
+				isAddingItem: false
+			};
+		},
+
+		mounted()
+		{
+			for (let index in this.flowData)
+				if (this.flowData.hasOwnProperty(index))
+					this.$set(this.hideMyBlock, index, true);
+		},
+
+		methods: {
+
+			key(item)
+			{
+				return JSON.stringify(item);
+			},
+
+			deleteItem(parentDOM, item, index)
+			{
+				this.flowData.splice(index, 1);
+				this.$emit("input", this.flowData);
+			},
+
+			toggleBlock(index)
+			{
+				this.$set(this.hideMyBlock, index, !this.hideMyBlock[index]);
+			},
+
+			addItem()
+			{
+				if (this.isAddingItem)
+					return;
+
+				this.isAddingItem = true;
+			},
+
+			cancelNewItem()
+			{
+				if (!this.isAddingItem)
+					return;
+
+				this.isAddingItem = false;
+			},
+
+			newItem(obj)
+			{
+				let oj = {
+					name: obj.key,
+					type: obj.type
+				};
+
+				if (obj.type === "array" || obj.type === "object")
+				{
+					oj.childParams = obj.value;
+					oj.remark = null;
+				}
+				else
+				{
+					oj.childParams = null;
+					oj.remark = obj.value;
+				}
+
+				if (!oj.name)
+				{
+					Latte.messages.alert(Latte.i18n.translate("root", "Invalid key"), Latte.i18n.translate("root", "Invalid key specified!"));
+					return;
+				}
+
+				this.flowData.push(oj);
+				this.$emit("input", this.flowData);
+				this.cancelNewItem();
+			},
+
+			keyInputBlur(item, e)
+			{
+				if (item.name.length <= 0)
+				{
+					Latte.messages.alert(Latte.i18n.translate("root", "Invalid key"), Latte.i18n.translate("root", "Invalid key specified!"));
+					item.name = "null";
+					e.target.focus();
+				}
+			}
+
+		}
+
+	}
+
+</script>
