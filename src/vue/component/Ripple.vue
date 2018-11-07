8516be2e6e20ce1f86645223960805b9b86b76ee
Bas Milius <basmilius@hotmail.com>
2018-11-07 23:14:50 +0100

    Finalized initial version of Ripple. Closes #4

--- src/vue/component/Ripple.vue
+++ src/vue/component/Ripple.vue
@@ -37,6 +37,7 @@
 				type: Boolean
 			},
 
+			// TODO(Bas): Implement centered ripple.
 			rippleCentered: {
 				default: false,
 				required: false,
@@ -67,6 +68,8 @@
 			this.container.classList.add("ripple-container");
 			this.$el.prepend(this.container);
 
+			this.$el.addEventListener("pointercancel", evt => this.onPointerCancel(evt), {passive: true});
+			this.$el.addEventListener("pointerout", evt => this.onPointerCancel(evt), {passive: true});
 			this.$el.addEventListener("pointerdown", evt => this.onPointerDown(evt), {passive: true});
 			this.$el.addEventListener("pointerup", evt => this.onPointerUp(evt), {passive: true});
 		},
@@ -94,11 +97,20 @@
 				ripple.classList.add("ripple");
 				ripple.style.setProperty("top", `${y - 18}px`);
 				ripple.style.setProperty("left", `${x - 18}px`);
-				ripple.style.setProperty("transition-duration", `90ms, ${this.rippleDuration + 180}ms`);
+				ripple.style.setProperty("--ripple-duration-private", `${this.rippleDuration + 180}ms`);
 
 				this.container.appendChild(ripple);
 
 				return ripple;
+			},
+
+			onPointerCancel()
+			{
+				if (this.preparedRipple !== null)
+					this.preparedRipple.remove();
+
+				this.isBackgroundVisible = false;
+				this.preparedRipple = null;
 			},
 
 			onPointerDown(evt)
@@ -130,15 +142,10 @@
 
 				ripple.style.setProperty("top", `${y - 18}px`);
 				ripple.style.setProperty("left", `${x - 18}px`);
+				ripple.classList.add("is-scaling");
 				ripple.classList.add("is-visible");
 
-				this.$nextTick(() => ripple.classList.add("is-scaling"));
-
-				timeout(this.rippleDuration, () =>
-				{
-					ripple.style.setProperty("transition-duration", `180ms, ${this.rippleDuration + 180}ms`);
-					ripple.classList.add("is-hiding");
-				});
+				timeout(this.rippleDuration, () => ripple.classList.add("is-hiding"));
 				timeout(this.rippleDuration + 180, () => ripple.remove());
 			}
 
@@ -159,59 +166,3 @@
 	}
 
 </script>
-
-<style lang="scss">
-
-	div.ripple-container
-	{
-		--ripple-color-private: var(--ripple-color, var(--color-dark));
-		--ripple-scale-private: var(--ripple-scale, 6);
-
-		position: absolute;
-		display: block;
-		contain: paint;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background: transparent;
-		border-radius: inherit;
-		overflow: hidden;
-		pointer-events: none;
-		transition: background 180ms var(--ease-swift-out);
-		will-change: background;
-
-		&.is-background-visible
-		{
-			background: rgba(var(--ripple-color-private), .05);
-		}
-
-		div.ripple
-		{
-			position: absolute;
-			display: block;
-			height: 36px;
-			width: 36px;
-			background: rgba(var(--ripple-color-private), .05);
-			border-radius: 18px;
-			opacity: 0;
-			transition: opacity var(--ease-swift-out), transform var(--ease-standard-curve);
-
-			&.is-visible
-			{
-				opacity: 1;
-			}
-
-			&.is-hiding
-			{
-				opacity: 0;
-			}
-
-			&.is-scaling
-			{
-				transform: scale3d(var(--ripple-scale-private), var(--ripple-scale-private), var(--ripple-scale-private));
-			}
-		}
-	}
-
-</style>
