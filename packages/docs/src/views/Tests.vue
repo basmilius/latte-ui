c9913fe3feac58cd339443e2f45918a0aea5d9ec
Bas Milius <basmilius@hotmail.com>
2019-03-02 15:49:51 +0100

    [docs] More documentation.

--- packages/docs/src/views/Tests.vue
+++ packages/docs/src/views/Tests.vue
@@ -20,59 +20,6 @@
 			<div class="row">
 				<div class="col-12">
 
-					<div class="row mb-panel-gutter">
-						<div class="col-12 col-lg-6">
-
-							<div class="panel">
-								<nav class="nav nav-list py-3" style="--nav-item-height: 60px">
-									<latte-ripple as="a" class="nav-link"><i class="mdi mdi-home"></i><span>Home</span></latte-ripple>
-
-									<latte-expandable>
-										<template v-slot:header="{isOpen}">
-											<latte-ripple as="a" class="nav-link">
-												<i class="mdi mdi-view-grid"></i>
-												<span>Components</span>
-												<i class="mdi mdi-chevron-up" v-if="isOpen"></i>
-												<i class="mdi mdi-chevron-down" v-else></i>
-											</latte-ripple>
-										</template>
-
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-									</latte-expandable>
-
-									<latte-expandable>
-										<template v-slot:header="{isOpen}">
-											<latte-ripple as="a" class="nav-link">
-												<i class="mdi mdi-language-javascript"></i>
-												<span>JavaScript</span>
-												<i class="mdi mdi-chevron-up" v-if="isOpen"></i>
-												<i class="mdi mdi-chevron-down" v-else></i>
-											</latte-ripple>
-										</template>
-
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-										<latte-ripple as="a" class="nav-link"><i class="mdi"></i><span>Home</span></latte-ripple>
-									</latte-expandable>
-								</nav>
-							</div>
-
-						</div>
-						<div class="col-12 col-lg-6">
-
-							<div class="panel">
-								<nav class="nav nav-list py-3">
-									<a class="nav-link"><i class="mdi mdi-home"></i><span>Home</span></a>
-								</nav>
-							</div>
-
-						</div>
-					</div>
-
 					<div class="panel">
 						<div class="panel-header"><span class="panel-title">Pagination</span></div>
 						<div class="panel-body">
@@ -85,33 +32,16 @@
 					</div>
 
 					<div class="panel">
-						<div class="panel-header"><span class="panel-title">Message dialog</span></div>
+						<div class="panel-header"><span class="panel-title">Snackbar</span></div>
 						<div class="panel-body">
 
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showAlert"><span>Show alert</span></latte-ripple>
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showConfirm"><span>Show confirm</span></latte-ripple>
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showPrompt"><span>Show prompt</span></latte-ripple>
+							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showSnackbar($latte.ui.snackbar.Locations.BOTTOM_LEFT)">Bottom left</latte-ripple>
+							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showSnackbar($latte.ui.snackbar.Locations.BOTTOM)">Bottom center</latte-ripple>
+							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showSnackbar($latte.ui.snackbar.Locations.BOTTOM_RIGHT)">Bottom right</latte-ripple>
 
-						</div>
-					</div>
-
-					<div class="panel">
-						<div class="panel-header"><span class="panel-title">Notification</span></div>
-						<div class="panel-body">
-
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showNotificationBasic"><span>Basic</span></latte-ripple>
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showNotificationIcon"><span>Icon</span></latte-ripple>
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showNotificationAvatar"><span>Avatar</span></latte-ripple>
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showNotificationButtons"><span>Buttons</span></latte-ripple>
-
-						</div>
-					</div>
-
-					<div class="panel">
-						<div class="panel-header"><span class="panel-title">Question dialog</span></div>
-						<div class="panel-body">
-
-							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showQuestion"><span>Open question</span></latte-ripple>
+							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showSnackbar($latte.ui.snackbar.Locations.TOP_LEFT)">Top left</latte-ripple>
+							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showSnackbar($latte.ui.snackbar.Locations.TOP)">Top center</latte-ripple>
+							<latte-ripple as="button" class="btn btn-contained btn-primary" @click="showSnackbar($latte.ui.snackbar.Locations.TOP_RIGHT)">Top right</latte-ripple>
 
 						</div>
 					</div>
@@ -144,69 +74,19 @@
 
 		methods: {
 
-			showAlert()
+			async showSnackbar(location)
 			{
-				this.$latte.ui.message.alert("Hello", "Hello world from the new Vue based message dialog!").then(result => console.log(result));
-			},
-
-			showConfirm()
-			{
-				this.$latte.ui.message.confirm("Hello", "Hello world from the new Vue based message dialog!").then(result => console.log(result));
-			},
-
-			showPrompt()
-			{
-				this.$latte.ui.message.prompt("Hello", "Hello world from the new Vue based message dialog!").then(result => console.log(result));
-			},
-
-			showNotificationAvatar()
-			{
-				this.$latte.ui.notification.create({
-					avatar: "https://latte.dev-preview.nl/module/@bas/website/resource/image/photos/2.jpg",
-					message: "<strong>Bas</strong> is now online"
-				});
-			},
-
-			showNotificationBasic()
-			{
-				this.$latte.ui.notification.create({
-					title: "I'm a notification",
-					message: "Lorem ipsum dolor sit amet consectetur..."
-				});
-			},
-
-			async showNotificationButtons()
-			{
-				const result = await this.$latte.ui.notification.create({
-					duration: 0,
-					avatar: "https://latte.dev-preview.nl/module/@bas/website/resource/image/photos/2.jpg",
-					title: "Incoming call",
-					message: "<strong>Bas</strong> is calling...",
-					buttons: [
-						{id: 1, icon: "phone", label: "Answer", color: "success"},
-						{id: 2, icon: "phone-hangup", label: "Deny", color: "error"}
-					]
+				const result = await Latte.ui.snackbar.create({
+					action: {
+						color: "success",
+						label: "Dismiss"
+					},
+					duration: 10000,
+					location: location,
+					message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin elit non venenatis cursus."
 				});
 
 				console.log(result);
-			},
-
-			showNotificationIcon()
-			{
-				this.$latte.ui.notification.create({
-					icon: "map-marker-outline",
-					message: "<strong>Maps</strong> is using your devices's location"
-				});
-			},
-
-			showQuestion()
-			{
-				this.$latte.ui.question.create("map-marker-outline", "Allow <strong>Maps</strong> to access this device's location?", [
-					{id: 1, label: "Allow all the time"},
-					{id: 2, label: "Allow when the app in use"},
-					{id: 4, label: "Deny"},
-					{id: 8, label: "Deny & don't ask again"}
-				]).then(result => console.log(result));
 			}
 
 		}
--- packages/docs/src/views/docs/components/Notification.vue
+++ packages/docs/src/views/docs/components/Notification.vue
@@ -6,20 +6,127 @@
 			<h1>Notification</h1>
 		</PageHeader>
 
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-lg-9 mb-panel-gutter">
+
+					<CodeExample class="darker" :show-code="false" title="What is a Notification" url="/snippets/components/notification/what.html"></CodeExample>
+
+					<div class="panel">
+						<div class="panel-header"><h2 class="panel-title">Minimal</h2></div>
+						<div class="panel-body">
+							<button class="btn btn-contained btn-primary" @click="showMinimal"><span>Show notification</span></button>
+						</div>
+						<div class="panel-body">
+							<CodeSnippet lang="js" url="/snippets/components/notification/minimal.snippet"></CodeSnippet>
+						</div>
+					</div>
+
+					<div class="panel">
+						<div class="panel-header"><h2 class="panel-title">Avatar</h2></div>
+						<div class="panel-body">
+							<button class="btn btn-contained btn-primary" @click="showAvatar"><span>Show notification</span></button>
+						</div>
+						<div class="panel-body">
+							<CodeSnippet lang="js" url="/snippets/components/notification/avatar.snippet"></CodeSnippet>
+						</div>
+					</div>
+
+					<div class="panel">
+						<div class="panel-header"><h2 class="panel-title">Icon</h2></div>
+						<div class="panel-body">
+							<button class="btn btn-contained btn-primary" @click="showIcon"><span>Show notification</span></button>
+						</div>
+						<div class="panel-body">
+							<CodeSnippet lang="js" url="/snippets/components/notification/icon.snippet"></CodeSnippet>
+						</div>
+					</div>
+
+					<div class="panel">
+						<div class="panel-header"><h2 class="panel-title">Buttons</h2></div>
+						<div class="panel-body">
+							<button class="btn btn-contained btn-primary" @click="showButtons"><span>Show notification</span></button>
+						</div>
+						<div class="panel-body">
+							<CodeSnippet lang="js" url="/snippets/components/notification/buttons.snippet"></CodeSnippet>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-12 col-lg-3">
+
+					<TableOfContents></TableOfContents>
+
+				</div>
+			</div>
+		</div>
+
 	</div>
 
 </template>
 
 <script>
 
+	import CodeSnippet from "../../../components/CodeSnippet";
+	import CodeExample from "../../../components/CodeExample";
 	import PageHeader from "../../../components/PageHeader";
+	import TableOfContents from "../../../components/TableOfContents";
 
 	export default {
 
 		name: "Notification",
 
 		components: {
-			PageHeader
+			CodeExample,
+			CodeSnippet,
+			PageHeader,
+			TableOfContents
+		},
+
+		methods: {
+
+			async showAvatar()
+			{
+				await Latte.ui.notification.create({
+					avatar: "//latte.dev-preview.nl/module/@bas/website/resource/image/photos/2.jpg",
+					title: "Example",
+					message: "Lorem ipsum dolor sit amet..."
+				});
+			},
+
+			async showButtons()
+			{
+				const result = await Latte.ui.notification.create({
+					duration: 0,
+					icon: "package",
+					title: "Update available!",
+					message: "Would you like to update now?",
+					buttons: [
+						{id: 1, label: "Update now", color: "primary"},
+						{id: 2, label: "Later"}
+					]
+				});
+
+				console.log(`Button ID: ${result}`);
+			},
+
+			async showIcon()
+			{
+				await Latte.ui.notification.create({
+					color: "warning",
+					icon: "map-marker-outline",
+					message: "<strong>Maps</strong> is using your device's location"
+				});
+			},
+
+			async showMinimal()
+			{
+				await Latte.ui.notification.create({
+					title: "Hello world",
+					message: "I'm a fancy notification!"
+				});
+			}
+
 		}
 
 	}
