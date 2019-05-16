84661c3b86686a0ef8788b3bb911f7fc4ce27eb0
Bas Milius <basmilius@hotmail.com>
2019-05-16 23:30:35 +0200

    [docs] Code cleanup.

--- packages/docs/src/components/CodeExample.vue
+++ packages/docs/src/components/CodeExample.vue
@@ -1,24 +1,21 @@
 <template>
 
 	<div class="panel code-example-panel" :class="{'is-loading': isLoading}">
-		<span class="spinner spinner-primary"></span>
-		<div class="panel-header">
-			<h2 class="panel-title mb-0">{{ title }}</h2>
-			<button class="btn btn-text btn-icon btn-dark ml-auto" @click="loadSnippet"><i class="mdi mdi-refresh"></i></button>
-		</div>
-		<div label="Preview" class="code-example-preview">
+		<div class="panel-header"><h2 class="panel-title mb-0">{{ title }}</h2></div>
 
+		<div class="code-example-preview">
 			<component :is="component" v-if="component !== null"></component>
-
 		</div>
-		<div label="Code" class="code-example-code" v-if="showCode">
 
+		<div class="code-example-code" v-if="showCode">
 			<CodeSnippet lang="html" v-if="code">{{ code }}</CodeSnippet>
-
 		</div>
+
 		<div class="panel-body" v-if="$slots.default">
 			<slot></slot>
 		</div>
+
+		<span class="spinner spinner-primary"></span>
 	</div>
 
 </template>
@@ -27,8 +24,6 @@
 
 	import Vue from "vue";
 	import CodeSnippet from "./CodeSnippet";
-
-	import { Latte } from "@bybas/latte-ui";
 
 	export default {
 
