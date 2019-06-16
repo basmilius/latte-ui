c7045fe7c16440543d3322eb42112dab030121fd
Bas Milius <basmilius@hotmail.com>
2019-06-16 22:19:10 +0200

    [docs] Vars for panels.

--- packages/docs/src/views/docs/components/Panel.vue
+++ packages/docs/src/views/docs/components/Panel.vue
@@ -11,18 +11,16 @@
 				<div class="col-12 col-lg-9 mb-panel-gutter">
 
 					<CodeExample class="darker" title="Panel" url="/snippets/components/panel/basic.html"></CodeExample>
-
 					<CodeExample class="darker" title="Panel with footer" url="/snippets/components/panel/footer.html"></CodeExample>
-
 					<CodeExample class="darker" title="Panel with media" url="/snippets/components/panel/media.html"></CodeExample>
-
 					<CodeExample class="darker" title="Panel with notice" url="/snippets/components/panel/notice.html"></CodeExample>
-
 					<CodeExample class="darker" title="Panel with list" url="/snippets/components/panel/list.html"></CodeExample>
-
 					<CodeExample class="darker" title="Panel with nav" url="/snippets/components/panel/nav.html"></CodeExample>
-
 					<CodeExample class="darker" title="Panel with tabs" url="/snippets/components/panel/tabs.html"></CodeExample>
+
+					<div class="divider divider-horizontal docs-separator"></div>
+
+					<Variables :vars="panelVars"/>
 
 				</div>
 				<div class="col-12 col-lg-3">
@@ -40,8 +38,9 @@
 <script>
 
 	import CodeExample from "../../../components/CodeExample";
-	import TableOfContents from "../../../components/TableOfContents";
 	import PageHeader from "../../../components/PageHeader";
+	import TableOfContents from "../../../components/TableOfContents";
+	import Variables from "../../../components/Variables";
 
 	export default {
 
@@ -50,8 +49,41 @@
 		components: {
 			CodeExample,
 			PageHeader,
-			TableOfContents
+			TableOfContents,
+			Variables
+		},
+
+		data()
+		{
+			const computedStyle = window
+				.getComputedStyle(document.body);
+
+			return {
+				panelVars: [
+					{
+						name: "--panel-background",
+						type: "rgb",
+						default: computedStyle.getPropertyValue("--panel-background").split(",").map(c => parseInt(c))
+					},
+					{
+						name: "--panel-elevation",
+						type: "string",
+						default: computedStyle.getPropertyValue("--panel-elevation")
+					},
+					{
+						name: "--panel-elevation-hover",
+						type: "string",
+						default: computedStyle.getPropertyValue("--panel-elevation-hover")
+					},
+					{
+						name: "--panel-gutter",
+						type: "string",
+						default: computedStyle.getPropertyValue("--panel-gutter")
+					}
+				]
+			};
 		}
+
 	}
 
 </script>
