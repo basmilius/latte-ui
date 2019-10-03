7c3b58bfa4fa02a7418c7e872331b3d383f759b8
Bas Milius <basmilius@hotmail.com>
2019-10-03 09:42:04 +0200

    [docs] Form addons and colors.

--- packages/docs/src/views/docs/components/FormElements.vue
+++ packages/docs/src/views/docs/components/FormElements.vue
@@ -11,21 +11,15 @@
 				<div class="col-12 col-lg-9 mb-panel-gutter">
 
 					<CodeExample title="Field" url="/snippets/components/form-elements/field.html"></CodeExample>
-
-					<CodeExample title="Date/Time" url="/snippets/components/form-elements/datetime.html"></CodeExample>
-
 					<CodeExample title="Select" url="/snippets/components/form-elements/select.html"></CodeExample>
-
+					<CodeExample title="Date/Time" url="/snippets/components/form-elements/datetime.html"></CodeExample>
+					<CodeExample title="Addons" url="/snippets/components/form-elements/addons.html"></CodeExample>
+					<CodeExample title="Colors" url="/snippets/components/form-elements/colors.html"></CodeExample>
 					<CodeExample title="Password" url="/snippets/components/form-elements/password.html"></CodeExample>
-
 					<CodeExample title="Autocomplete" url="/snippets/components/form-elements/autocomplete.html"></CodeExample>
-
 					<CodeExample title="Range" url="/snippets/components/form-elements/range.html"></CodeExample>
-
 					<CodeExample title="Checkbox" url="/snippets/components/form-elements/checkbox.html"></CodeExample>
-
 					<CodeExample title="Radio" url="/snippets/components/form-elements/radio.html"></CodeExample>
-
 					<CodeExample title="Toggle" url="/snippets/components/form-elements/toggle.html"></CodeExample>
 
 				</div>
