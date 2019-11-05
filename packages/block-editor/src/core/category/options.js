52847bd5da7f53b2f87ae48e8523d75311397824
Bas Milius <basmilius@hotmail.com>
2019-11-05 15:22:21 +0100

    [block-editor] i18n

--- packages/block-editor/src/core/category/options.js
+++ packages/block-editor/src/core/category/options.js
@@ -1,6 +1,8 @@
+import { translate } from "../i18n";
+
 export const defaultCategories = [
-	{id: "layout", icon: "layers", title: "Layout", description: ""},
-	{id: "text", icon: "format-text", title: "Text", description: ""},
-	{id: "embeds", icon: "code-tags", title: "Embeds", description: ""},
-	{id: "other", icon: "package-variant", title: "Other", description: ""}
+	{id: "layout", icon: "layers", title: translate("Layout"), description: ""},
+	{id: "text", icon: "format-text", title: translate("Text"), description: ""},
+	{id: "embeds", icon: "code-tags", title: translate("Embeds"), description: ""},
+	{id: "other", icon: "package-variant", title: translate("Other"), description: ""}
 ];
