d15444ffdda50da4e83486ecbed7c284930c0548
Bas Milius <basmilius@hotmail.com>
2019-11-26 23:03:00 +0100

    [colorpicker] Made values changeable #53

--- packages/latte-ui/src/vue/component/ColorPickerSelect.vue
+++ packages/latte-ui/src/vue/component/ColorPickerSelect.vue
@@ -21,23 +21,23 @@
 		<div class="colorpicker-preview">
 			<div class="colorpicker-preview-color" :style="{backgroundColor: `rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${alpha / 100})`}"></div>
 			<label class="preview-value hex">
-				<input class="form-control" type="text" maxlength="7" :value="color.hex"/>
+				<input class="form-control" type="text" maxlength="7" v-model="hex" pattern="[a-fA-F0-9]+" @blur="onHueSaturationChanged" @keydown="onHexKeyDown" @input="onHexChange" ref="hex"/>
 				<span>HEX</span>
 			</label>
 			<label class="preview-value rgb">
-				<input class="form-control" type="number" min="0" max="255" :value="color.rgb.r"/>
+				<input class="form-control" type="number" :min="0" :max="255" :step="1" v-model="r" @blur="onHueSaturationChanged" @input="onRgbChange" ref="r"/>
 				<span>R</span>
 			</label>
 			<label class="preview-value rgb">
-				<input class="form-control" type="number" min="0" max="255" :value="color.rgb.g"/>
+				<input class="form-control" type="number" :min="0" :max="255" :step="1" v-model="g" @blur="onHueSaturationChanged" @input="onRgbChange" ref="g"/>
 				<span>G</span>
 			</label>
 			<label class="preview-value rgb">
-				<input class="form-control" type="number" min="0" max="255" :value="color.rgb.b"/>
+				<input class="form-control" type="number" :min="0" :max="255" :step="1" v-model="b" @blur="onHueSaturationChanged" @input="onRgbChange" ref="b"/>
 				<span>B</span>
 			</label>
 			<label class="preview-value alpha">
-				<input class="form-control" type="number" min="0" max="1" step="0.01" :value="(alpha / 100).toFixed(2)"/>
+				<input class="form-control" type="number" min="0" max="1" step="0.01" readonly :value="(alpha / 100).toFixed(2)"/>
 				<span>A</span>
 			</label>
 		</div>
@@ -48,8 +48,9 @@
 
 <script>
 
-	import { hsvToRGB, smartColor } from "../../js/util/color";
 	import { clamp, roundStep } from "../../js/math";
+	import { hsvToRGB, smartColor } from "../../js/util/color";
+	import { terminateEvent } from "../../js/util/dom";
 
 	import Slider from "./base/Slider";
 	import XYPos from "./base/XYPos";
@@ -64,12 +65,18 @@
 			value: {default: "#4752ba"}
 		},
 
+		refs: ["hex"],
+
 		data()
 		{
 			const color = smartColor(this.value);
 
 			return {
 				alpha: (color.alpha || 1) * 100,
+				hex: color.hex,
+				r: color.rgb.r,
+				g: color.rgb.g,
+				b: color.rgb.b,
 				hue: 0,
 				color: color,
 				saturation: {x: 0, y: 0},
@@ -93,6 +100,36 @@
 		},
 
 		methods: {
+
+			onHexChange()
+			{
+				let {value} = this.$refs.hex;
+
+				if (value.substring(0, 1) !== "#")
+					value = "#" + value;
+
+				if (value.length !== 4 && value.length !== 7)
+					return;
+
+				this.color = smartColor(value);
+			},
+
+			onRgbChange()
+			{
+				this.color = smartColor({
+					r: clamp(this.r, 0, 255),
+					g: clamp(this.g, 0, 255),
+					b: clamp(this.b, 0, 255)
+				}, this.oldHue, this.alpha / 100);
+			},
+
+			onHexKeyDown(evt)
+			{
+				if (evt.key.match(/[0-9a-f]+/))
+					return;
+
+				terminateEvent(evt);
+			},
 
 			onHueSaturationChanged()
 			{
@@ -118,6 +155,10 @@
 					this.isChanging = true;
 
 					this.alpha = this.color.alpha * 100;
+					this.hex = this.$refs.hex === document.activeElement ? this.hex : this.color.hex;
+					this.r = this.$refs.r === document.activeElement ? this.r : this.color.rgb.r;
+					this.g = this.$refs.g === document.activeElement ? this.g : this.color.rgb.g;
+					this.b = this.$refs.b === document.activeElement ? this.b : this.color.rgb.b;
 					this.hue = roundStep(this.color.hsv.h * 360, 1);
 					this.saturation = {
 						x: this.color.hsv.s,
