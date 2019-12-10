b585f6d4180b2539fafc3f822961001315c19ca9
Bas Milius <basmilius@hotmail.com>
2019-12-10 14:15:12 +0100

    [docs] Removed notice

--- packages/docs/src/components/PageHeader.vue
+++ packages/docs/src/components/PageHeader.vue
@@ -8,11 +8,6 @@
 						<slot></slot>
 					</div>
 				</div>
-				<div class="col-12 col-lg-4 my-3 my-lg-0">
-					<div class="notice notice-outline notice-info">
-						<p>Note: This documentation website is a work in progress! A lot is missing!</p>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
