b165b8bc64987f663b141706782ea1cbece3130c
Teun Strik <teunstrik@yahoo.nl>
2019-12-10 12:19:28 +0100

    Added overlay example

--- packages/docs/src/views/docs/components/Overlay.vue
+++ packages/docs/src/views/docs/components/Overlay.vue
@@ -6,6 +6,21 @@
 			<h1>Overlay</h1>
 		</PageHeader>
 
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-lg-9 mb-panel-gutter">
+
+					<CodeExample url="/snippets/components/overlay/basic.html"></CodeExample>
+
+				</div>
+				<div class="col-12 col-lg-3">
+
+					<TableOfContents></TableOfContents>
+
+				</div>
+			</div>
+		</div>
+
 	</div>
 
 </template>
@@ -13,15 +28,18 @@
 <script>
 
 	import PageHeader from "../../../components/PageHeader";
+	import TableOfContents from "../../../components/TableOfContents";
+	import CodeExample from "../../../components/CodeExample";
 
 	export default {
 
 		name: "Overlay",
 
 		components: {
+			CodeExample,
+			TableOfContents,
 			PageHeader
 		}
-
 	}
 
 </script>
