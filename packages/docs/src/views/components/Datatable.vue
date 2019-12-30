eb455fe64f5bbff3cd7749005fdd80325dd66b46
Bas Milius <basmilius@hotmail.com>
2019-12-30 21:27:45 +0100

    [docs] Added api documentation for datatable.

--- packages/docs/src/views/components/Datatable.vue
+++ packages/docs/src/views/components/Datatable.vue
@@ -18,6 +18,8 @@
 
 		<TitledRow class="py-5 text-center text-lg-left" title="API" id="api">
 			<div class="col-12">
+				<ApiExplorer v-bind="api"/>
+
 				<div class="panel">
 					<div class="panel-header"><span class="panel-title">Data source</span></div>
 					<div class="panel-body">
@@ -37,16 +39,82 @@
 	import TitledRow from "../../components/TitledRow";
 	import CodeExample from "../../components/CodeExample";
 	import CodeSnippet from "../../components/CodeSnippet";
+	import ApiExplorer from "../../components/ApiExplorer";
 
 	export default {
 
 		name: "Chat",
 
-		components: {CodeSnippet, CodeExample, TitledRow, PageHeader},
+		components: {ApiExplorer, CodeSnippet, CodeExample, TitledRow, PageHeader},
 
 		data()
 		{
 			return {
+				api: {
+					events: [
+						{
+							name: "input",
+							description: "Invoked when the selection of the data table is changed.",
+							signature: "(selection: Array | number)"
+						}
+					],
+					properties: [
+						{
+							name: "data-source",
+							description: "When a function is passed, that function should return a data source object described below. When an URL is given that is used as a data source.",
+							default: null,
+							type: "Function, string"
+						},
+						{
+							name: "default-limit",
+							description: "The initial limit of items to load and show. This property is used when the datatable is mounted.",
+							default: "20",
+							type: "number"
+						},
+						{
+							name: "name",
+							description: "The name of the datatable within a <form> element.",
+							default: "[random]",
+							type: "string"
+						},
+						{
+							name: "select-mode",
+							description: "The selection mode used in the datatable. When single is used, value is a number containing an ID and when multiple is used value is an array containing the IDs.",
+							default: "none",
+							type: "'none', 'single', 'multiple'"
+						},
+						{
+							name: "show-header",
+							description: "Enable or disable the table header.",
+							default: true,
+							type: "boolean"
+						},
+						{
+							name: "show-search",
+							description: "Enable or disable searching the data.",
+							default: true,
+							type: "boolean"
+						},
+						{
+							name: "show-sorting",
+							description: "Enable or disable data sorting.",
+							default: true,
+							type: "boolean"
+						},
+						{
+							name: "spinner",
+							description: "Should the datatable add a spinner when data is loading?",
+							default: true,
+							type: "boolean"
+						},
+						{
+							name: "value",
+							description: "The selection value.",
+							default: null,
+							type: "Array, number, NULL"
+						}
+					]
+				},
 				bindings: {
 					standard: {
 						exampleDataSource: require("../../data/components/datatable/data-source.js").default
