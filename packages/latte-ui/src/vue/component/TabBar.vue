32b162b8e95ac42cd8d0694ef791dd7625dfa394
Bas Milius <basmilius@hotmail.com>
2020-01-30 21:17:55 +0100

    [tabs] Removed support for animated indicators.

--- packages/latte-ui/src/vue/component/TabBar.vue
+++ packages/latte-ui/src/vue/component/TabBar.vue
@@ -12,23 +12,18 @@
 	<nav class="nav nav-tabs">
 
 		<template v-for="(tab, index) in tabs">
-			<a class="nav-link" :class="{'is-active': tab.active, 'no-indicator': animatedIndicator}" @click="click(index)">
+			<a class="nav-link" :class="{'is-active': tab.active}" @click="click(index)">
 				<Icon :name="tab.icon" v-if="tab.icon !== ''"/>
 				<span v-if="tab.label !== ''">{{ tab.label }}</span>
 				<span class="badge badge-primary ml-2" v-if="tab.badge !== ''">{{ tab.badge }}</span>
 			</a>
 		</template>
 
-		<div class="nav-tabs-indicator" :style="indicatorStyles" v-if="animatedIndicator"></div>
-
 	</nav>
 
 </template>
 
 <script>
-
-	import { raf } from "../../js/util/dom";
-	import { on } from "../../js/core/action";
 
 	import Icon from "./Icon.vue";
 
@@ -40,51 +35,18 @@
 
 		inject: ["tabContainer"],
 
-		props: {
-			animatedIndicator: {default: false, type: Boolean}
-		},
-
 		data()
 		{
 			return {
-				subscriptions: [],
 				current: 0,
-				indicatorBarRect: null,
-				indicatorTabRect: null,
 				tabs: []
 			};
-		},
-
-		destroyed()
-		{
-			this.subscriptions.forEach(sub => sub.unsubscribe());
 		},
 
 		mounted()
 		{
 			this.tabContainer.$on("change", current => this.onTabChange(current));
 			this.tabContainer.updateTabBars();
-
-			this.subscriptions.push(
-				on("latte:tick", () => raf(() => this.updateIndicator()))
-			);
-
-			raf(() => this.updateIndicator());
-		},
-
-		computed: {
-
-			indicatorStyles()
-			{
-				if (this.indicatorBarRect === null || this.indicatorTabRect === null)
-					return null;
-
-				return {
-					width: `${Math.round(this.indicatorTabRect.width)}px`,
-					transform: `translate3d(${Math.round(this.indicatorTabRect.left - this.indicatorBarRect.left)}px, 0, 0)`
-				};
-			}
-
 		},
 
 		methods: {
@@ -94,44 +56,9 @@
 				this.tabContainer.current = index;
 			},
 
-			updateIndicator()
-			{
-				this.indicatorTabRect = null;
-
-				if (!this.animatedIndicator)
-					return;
-
-				const tab = this.$el.querySelector(`a.nav-link:nth-child(${this.current + 1})`);
-
-				if (tab === null)
-					return;
-
-				this.indicatorBarRect = this.$el.getBoundingClientRect();
-				this.indicatorTabRect = tab.getBoundingClientRect();
-			},
-
 			onTabChange(current)
 			{
 				this.current = current;
-			}
-
-		},
-
-		watch: {
-
-			animatedIndicator()
-			{
-				raf(() => this.updateIndicator());
-			},
-
-			current()
-			{
-				raf(() => this.updateIndicator());
-			},
-
-			tabs()
-			{
-				raf(() => this.updateIndicator());
 			}
 
 		}
